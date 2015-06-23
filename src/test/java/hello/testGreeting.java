cc3caefadafae90f3d45c9fe6ec2ee87f6443d62
vincentfree <vinsterdamus@gmail.com>
2015-06-23 09:07:25 +0200

    Update testGreeting.java

--- src/test/java/hello/testGreeting.java
+++ src/test/java/hello/testGreeting.java
@@ -9,6 +9,10 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by Vincent on 21-6-2015.
  */
+@RunWith(SpringJUnit4ClassRunner.class)   // 1
+@SpringApplicationConfiguration(classes = Application.class)   // 2
+@WebAppConfiguration   // 3
+@IntegrationTest("server.port:0")   // 4
 public class testGreeting {
     Greeting greeting = new Greeting(1l,"Vincent Free");
 
