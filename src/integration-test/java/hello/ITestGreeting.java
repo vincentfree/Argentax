ae1dd74493014f7ddc132627de7fa342ba18ee7a
Vincent Free <neromy_1@msn.com>
2015-07-04 10:39:45 +0200

    integration-test toegevoegd

--- src/integration-test/java/hello/ITestGreeting.java
+++ src/integration-test/java/hello/ITestGreeting.java
@@ -1,6 +1,35 @@
 package hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
+
 /**
- * Created by VincentFree on 4-7-2015.
+ * Created by Vincent on 21-6-2015.
  */
+@RunWith(SpringJUnit4ClassRunner.class)   // 1
+@SpringApplicationConfiguration(classes = SampleController.class)   // 2
+//@IfProfileValue(name="test-groups", values={"unit-tests", "integration-tests"})
+@WebAppConfiguration   // 3
+//@IntegrationTest("server.port:1")   // 4
 public class ITestGreeting {
+    Greeting greeting = new Greeting(1l, "Vincent Free");
+    //GreetingController greetingController = new GreetingController();
+
+    @Test
+    public void testId() {
+        //SpringApplication.run(Application.class);
+        assertEquals(1L, greeting.getId());
+    }
+
+    @Test
+    public void testContent() {
+        //SpringApplication.run(Application.class);
+        assertEquals("Vincent Free", greeting.getContent());
+    }
 }
--- src/integration-test/java/hello/ITestGreetingController.java
+++ src/integration-test/java/hello/ITestGreetingController.java
@@ -1,6 +1,29 @@
 package hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
+
 /**
- * Created by VincentFree on 4-7-2015.
+ * Created by Vincent on 21-6-2015.
  */
+@RunWith(SpringJUnit4ClassRunner.class)   // 1
+@SpringApplicationConfiguration(classes = SampleController.class)   // 2
+//@IfProfileValue(name="test-groups", values={"unit-tests", "integration-tests"})
+@WebAppConfiguration   // 3
+//@IntegrationTest("server.port:1")   // 4
 public class ITestGreetingController {
+    GreetingController greetingController = new GreetingController();
+
+    @Test
+    public void testGreeting() {
+        greetingController.greeting("test");
+        assertEquals(1L, new GreetingController().greeting("f").getId());
+        assertEquals("Hello testmessage!", new GreetingController().greeting("testmessage").getContent());
+    }
 }
