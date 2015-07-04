3f120d65e6da663b3e05c083365e581b98056bb2
Vincent <vinsterdamus@gmail.com>
2015-07-04 12:59:39 +0200

    all en remove rest calls aangemaakt.

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,8 +2,11 @@ package hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -15,9 +18,20 @@ public class GreetingController {
 
     private static final String template = "Hello %s!";
     private final AtomicLong counter = new AtomicLong();
+    private static ArrayList<Greeting> greetings = new ArrayList<>();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        greetings.add(new Greeting(counter.incrementAndGet(), String.format(template, name)));
+        return greetings.get(greetings.size()-1);
+    }
+
+    @RequestMapping("/all")
+    public ArrayList all() {
+        return greetings;
+    }
+    @RequestMapping("/removeall")
+    public void removeAll(){
+        greetings.clear();
     }
 }
