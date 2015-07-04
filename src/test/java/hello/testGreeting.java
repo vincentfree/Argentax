cb1078b92e4836d6e16a736e037b4ab8798f6bd7
Vincent Free <neromy_1@msn.com>
2015-07-04 10:14:00 +0200

    integration-test toegevoegd om te kijken of het opgepakt wordt door circle-ci. Verder config voor heroku aangepast

--- src/test/java/hello/testGreeting.java
+++ src/test/java/hello/testGreeting.java
@@ -1,15 +1,6 @@
 package hello;
 
 import org.junit.Test;
-import org.junit.Assert.*;
-import org.junit.runner.RunWith;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.annotation.IfProfileValue;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 
 import static org.junit.Assert.assertEquals;
@@ -17,24 +8,17 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by Vincent on 21-6-2015.
  */
-@RunWith(SpringJUnit4ClassRunner.class)   // 1
-@SpringApplicationConfiguration(classes = SampleController.class)   // 2
-//@IfProfileValue(name="test-groups", values={"unit-tests", "integration-tests"})
-@WebAppConfiguration   // 3
-@IntegrationTest("server.port:1")   // 4
 public class testGreeting {
     Greeting greeting = new Greeting(1l, "Vincent Free");
     //GreetingController greetingController = new GreetingController();
 
     @Test
     public void testId() {
-        //SpringApplication.run(Application.class);
         assertEquals(1L, greeting.getId());
     }
 
     @Test
     public void testContent() {
-        //SpringApplication.run(Application.class);
         assertEquals("Vincent Free", greeting.getContent());
     }
 }
